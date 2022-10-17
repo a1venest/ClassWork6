19609e53ae3d867ea8746320e3542992314b3348
Aivenest <aivenest@gmail.com>
2022-10-17 21:25:49 +0500

    app3

---
+++ app3/Program.cs
@@ -0,0 +1,77 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите
+// элементы, у которых оба индекса чётные, и
+// замените эти элементы на их квадраты.
+// Например, изначально массив
+// выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Новый массив будет выглядеть
+// вот так:
+// 1 4 7 2
+// 5 81 2 9
+// 8 4 2 4
+
+
+int GetNumberFromConsole(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetNumberValue(int minValue, int maxValue)
+{
+    return new Random().Next(minValue, maxValue + 1);
+}
+
+void FillArray(int[,] arr, int minValue, int maxValue)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = GetNumberValue(minValue, maxValue);
+        }
+    }
+}
+
+void FillArrayTwo(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if ((i + 1) % 2 == 0 && (j + 1) % 2 == 0) arr[i, j] = Convert.ToInt32(Math.Pow(arr[i, j], 2));
+        }
+    }
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] InitializateArray(int m, int n)
+{
+    return new int[m, n];
+}
+
+int m = GetNumberFromConsole("Введите размерность массива: m");
+int n = GetNumberFromConsole("Введите размерность массива: n");
+int[,] array = InitializateArray(m, n);
+int maxValue = GetNumberFromConsole("Введите максимальное допустимое число в массиве:");
+int minValue = GetNumberFromConsole("Введите минимальное допустимое число в массиве:");
+FillArray(array, minValue, maxValue);
+PrintArray(array);
+Console.WriteLine();
+FillArrayTwo(array);
+PrintArray(array);
+
+
